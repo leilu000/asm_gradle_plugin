b0697bf18521052ebdee6a71f4f4dd68bd4d3394
leilu1 <880519Leilu123>
2022-05-25 19:17:16 +0800

    修改module的结构，方便查看

--- moudles/app/src/androidTest/java/com/leilu/asm/gradle/plugin/ExampleInstrumentedTest.kt
+++ moudles/app/src/androidTest/java/com/leilu/asm/gradle/plugin/ExampleInstrumentedTest.kt
@@ -3,7 +3,6 @@ package com.leilu.asm.gradle.plugin
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
